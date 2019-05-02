4d73b4979f90025224d5bf3e1509cf94408b4358
dawimuhammad <muhamadhaddawi@gmail.com>
2019-05-02 16:20:23 +0700

    Variables type declarations and remove unused go file

---
+++ four/variables.go
@@ -0,0 +1,50 @@
+// Learn Go Programming Course
+// Variable in Golang
+//
+// Copyright © 2019 dawimuhammad
+
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Print the literals
+//
+//  1. Print a few integer literals
+//
+//  2. Print a few float literals
+//
+//  3. Print true and false bool literals
+//
+//  4. Print your name using a string literal
+//
+//  5. Print a non-english sentence using a string literal
+//
+// ---------------------------------------------------------
+
+import "fmt"
+
+func main() {
+	// integer literals
+	fmt.Println(
+		100, -2, 10, 101,
+	)
+
+	// float literals
+	fmt.Println(
+		-.5, 100., -1.,
+	)
+
+	// bool literals
+	fmt.Println(
+		true,
+		false,
+	)
+
+	// string literals
+	fmt.Println(
+		"",
+		"Hi",
+		"Muhamad Haddawi",
+	)
+
+	fmt.Println(0x96)
+}
